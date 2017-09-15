6d241289eff49093804e19a5940f7172f4291395
Rafael Souza <rafael.souza@accesstage.com.br>
2017-09-14 23:58:29 -0300

    Ajustes

--- crm-server/src/main/java/com/crafaelsouza/crm/rest/CustomerController.java
+++ crm-server/src/main/java/com/crafaelsouza/crm/rest/CustomerController.java
@@ -3,6 +3,7 @@ package com.crafaelsouza.crm.rest;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,13 @@ import com.crafaelsouza.crm.dto.CustomerDTO;
 @RequestMapping(path = "/customer")
 public class CustomerController {
 
+	@Value("${parameters.teste}")
+	private String teste;
+	
 	@GetMapping
 	public List<CustomerDTO> findAll() {
 		CustomerDTO customer = new CustomerDTO();
-		customer.setFirstName("Joseph");
-		customer.setLastName("Conway");
+		customer.setFirstName(teste);
 		customer.setEmail("joseph@teste.com");
 		customer.setPhone("2131232");
 		
